48ab0ba849bfc70429962d8a10a86fe799ec5ff7
sylas08 <lundonk2025@masseyhigh.school.nz>
2026-05-21 10:01:19 +1200

    Start GUI completed

---
+++ B_01_Country_Capitals_V1.py
@@ -0,0 +1,82 @@
+import csv
+import random
+from tkinter import *
+from functools import partial  # To prevent unwanted windows
+
+
+# Make sure when a user is searching for capital cities we make sure it's not a non-capital city
+non_capital_cities = ["New York", "Los Angeles", "Chicago", "Houston", "Phoenix",
+   "Miami", "Toronto", "Vancouver", "Montreal", "Calgary",
+   "Sydney", "Melbourne", "Brisbane", "Perth", "Adelaide",
+   "Manchester", "Birmingham", "Liverpool", "Glasgow",
+   "Marseille", "Lyon", "Toulouse", "Nice", "Nantes",
+   "Hamburg", "Munich", "Cologne", "Frankfurt", "Stuttgart",
+   "Milan", "Naples", "Turin", "Palermo", "Genoa",
+   "Barcelona", "Valencia", "Seville", "Zaragoza", "Málaga",
+   "Porto", "Funchal", "Braga", "Setúbal",
+   "Thessaloniki", "Patras", "Larissa", "Heraklion", "Volos",
+   "Yokohama", "Osaka", "Nagoya", "Sapporo", "Kobe",
+   "Shanghai", "Guangzhou", "Shenzhen", "Chongqing", "Chengdu",
+   "Mumbai", "Bangalore", "Hyderabad", "Ahmedabad",
+   "Rio de Janeiro", "São Paulo", "Salvador", "Fortaleza", "Belo Horizonte",
+   "Medellin", "Cali", "Barranquilla", "Cartagena",
+   "Lima", "Arequipa", "Trujillo", "Chiclayo", "Iquitos",
+   "Buenos Aires", "Córdoba", "Rosario", "Mendoza", "La Plata",
+   "Auckland", "Christchurch", "Hamilton", "Tauranga",
+   "Dubai", "Sharjah", "Al Ain", "Ajman", "Istanbul",
+   "Izmir", "Bursa", "Antalya", "Jeddah", "Mecca",
+   "Medina", "Dammam", "Durban", "Johannesburg",
+   "Mombasa", "Kisumu", "Nakuru", "Eldoret", "Lagos",
+   "Kano", "Ibadan", "Kaduna", "Port Harcourt", "Casablanca",
+   "Fes", "Marrakech", "Tangier", "Accra", "Kumasi",
+   "Tamale", "Sekondi-Takoradi", "Cape Coast", "Ho Chi Minh City", "Da Nang",
+   "Hai Phong", "Can Tho", "Chiang Mai", "Pattaya", "Phuket",
+   "Khon Kaen", "Penang", "Johor Bahru", "Ipoh", "Kuching",
+    "Surabaya", "Bandung", "Medan", "Semarang"]
+
+# The capital cities for the quiz when the user is guessing
+capital_cities = {
+    "Paris": "France", "Berlin": "Germany", "Rome": "Italy",
+    "Madrid": "Spain", "Lisbon": "Portugal", "Athens": "Greece",
+    "Tokyo": "Japan",  "Beijing": "China", "New Delhi": "India",
+    "Brasília": "Brazil", "Canberra": "Australia", "Ottawa": "Canada",
+    "Washington, D.C.": "United States",  "Moscow": "Russia", "London": "United Kingdom",
+    "Cairo": "Egypt", "Pretoria": "South Africa", "Nairobi": "Kenya",
+    "Buenos Aires": "Argentina", "Lima": "Peru", "Oslo": "Norway",
+    "Stockholm": "Sweden", "Helsinki": "Finland", "Copenhagen": "Denmark",
+    "Wellington": "New Zealand", "Vienna": "Austria", "Brussels": "Belgium",
+    "Amsterdam": "Netherlands", "Dublin": "Ireland", "Seoul": "South Korea",
+    "Bangkok": "Thailand", "Kuala Lumpur": "Malaysia", "Jakarta": "Indonesia",
+    "Singapore": "Singapore", "Ankara": "Turkey", "Riyadh": "Saudi Arabia",
+    "Abu Dhabi": "United Arab Emirates", "Tehran": "Iran", "Baghdad": "Iraq",
+    "Jerusalem": "Israel", "Mexico City": "Mexico", "Santiago": "Chile",
+    "Bogotá": "Colombia", "Quito": "Ecuador", "Caracas": "Venezuela",
+    "Sucre": "Bolivia", "Asunción": "Paraguay", "Montevideo": "Uruguay",
+    "Georgetown": "Guyana", "Havana": "Cuba", "Santo Domingo": "Dominican Republic",
+    "San Salvador": "El Salvador", "Guatemala City": "Guatemala", "Tegucigalpa": "Honduras",
+    "Managua": "Nicaragua", "San José": "Costa Rica", "Panama City": "Panama",
+    "Kingston": "Jamaica", "Nassau": "Bahamas", "Port-au-Prince": "Haiti",
+    "Philipsburg": "Sint Maarten", "Reykjavik": "Iceland", "Warsaw": "Poland",
+    "Budapest": "Hungary", "Bucharest": "Romania", "Sofia": "Bulgaria",
+    "Zagreb": "Croatia", "Ljubljana": "Slovenia", "Bratislava": "Slovakia",
+    "Prague": "Czech Republic", "Belgrade": "Serbia", "Sarajevo": "Bosnia and Herzegovina",
+    "Podgorica": "Montenegro", "Pristina": "Kosovo", "Skopje": "North Macedonia",
+    "Tirana": "Albania", "Valletta": "Malta", "Nicosia": "Cyprus",
+    "Yerevan": "Armenia", "Baku": "Azerbaijan", "Tbilisi": "Georgia",
+    "Kiev": "Ukraine", "Minsk": "Belarus", "Vilnius": "Lithuania",
+    "Riga": "Latvia", "Tallinn": "Estonia", "Hanoi": "Vietnam",
+    "Phnom Penh": "Cambodia", "Vientiane": "Laos", "Rangoon": "Myanmar",
+    "Manila": "Philippines", "Kathmandu": "Nepal", "Colombo": "Sri Lanka",
+    "Dhaka": "Bangladesh", "Thimphu": "Bhutan", "Male": "Maldives",
+    "Islamabad": "Pakistan", "Kabul": "Afghanistan", "Tashkent": "Uzbekistan",
+    "Dushanbe": "Tajikistan", "Ashgabat": "Turkmenistan", "Bishkek": "Kyrgyzstan",
+    "Nur-Sultan": "Kazakhstan", "Ulaanbaatar": "Mongolia", "Pyongyang": "North Korea",
+    "Bandar Seri Begawan": "Brunei", "Taipei": "Taiwan", "Hong Kong": "Hong Kong",
+    "Macau": "Macau", "Rabat":"Morocco", "Abuja": "Nigeria", "Accra": "Ghana", "Addis Ababa": "Ethiopia",
+    "Doha": "Qatar", "Damascus": "Syria", "Amman": "Jordan",
+    "Bern": "Switzerland", "Andorra la Vella": "Andorra",
+    "Suva": "Fiji", "Port Moresby": "Papua New Guinea",
+    "Bridgetown": "Barbados", "Castries": "Saint Lucia"
+}
+
+# Complete the quiz here !
